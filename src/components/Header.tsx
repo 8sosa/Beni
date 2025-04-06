f3d79028607bd8c05db9791fc9124d7e996659b6
8sosa <emmanuel.amos@stu.cu.edu.ng>
2025-04-07 00:05:02 +0100

    update 1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import React, { useState, useEffect, useRef } from "react";
+import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import NavLink from "./NavLink";
+import { HiOutlineMenuAlt4, HiOutlineX } from "react-icons/hi";
+import { CiSearch } from "react-icons/ci";
+import { BsBag } from "react-icons/bs";
 import "./style.css";
 
 export default function Header() {
@@ -10,9 +14,9 @@ export default function Header() {
   const [portfolioLocked, setPortfolioLocked] = useState(false);
   const [servicesOpen, setServicesOpen] = useState(false);
   const [servicesLocked, setServicesLocked] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   const [scrolled, setScrolled] = useState(false);
   const [loading, setLoading] = useState(false);
-
   const router = useRouter();
   const pathname = usePathname();
   const portfolioRef = useRef<HTMLDivElement>(null);
@@ -64,6 +68,7 @@ export default function Header() {
   const handleNavigation = (href: string) => {
     setLoading(true);
     router.push(href);
+    setMenuOpen(false);
   };
 
   return (
@@ -163,6 +168,92 @@ export default function Header() {
               About Me
             </NavLink>
             <NavLink href="/contact" onClick={() => handleNavigation("/contact")} className="navLink">
+              Contact
+            </NavLink>
+          </nav>
+        </div>
+
+        {/* Mobile Navigation */}
+        <div className="mx-auto max-w-7xl px-4 py-3 flex items-center justify-between lg:hidden">
+          <div className="text-2xl md:text-3xl font-bold" onClick={() => handleNavigation("/")} >BENI.</div>
+          <nav className="title flex space-x-6">
+            <button
+              className="text-3xl focus:outline-none"
+              onClick={() => setMenuOpen(true)}
+              aria-label="Open Menu"
+            >
+              <HiOutlineMenuAlt4 />
+            </button>
+            <Link
+              href="#"
+              className="text-gray-700 hover:text-black transition-colors"
+            >
+              <BsBag size={24} />
+            </Link>
+            <Link
+              href="#"
+              className="text-gray-700 hover:text-black transition-colors"
+            >
+              <CiSearch size={24} />
+            </Link>
+          </nav>
+        </div>
+        {/* Off-Canvas Menu */}
+        <div
+          className={`fixed top-0 right-0 h-screen w-full bg-black transform 
+            ${menuOpen ? "translate-x-0" : "translate-x-full"} 
+            transition-transform duration-300 ease-in-out z-50`}
+        >
+          <div className="flex items-center justify-end px-4 py-3">
+            <button
+              className="text-3xl focus:outline-none text-white"
+              onClick={() => setMenuOpen(false)}
+              aria-label="Close Menu"
+            >
+              <HiOutlineX />
+            </button>
+          </div>
+          <nav className="flex flex-col p-4 space-y-4 text-center items-end title">
+            <NavLink
+              href="/"
+              className="navLink"
+              onClick={() => handleNavigation("/")}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              href="/portfolio"
+              className="navLink"
+              onClick={() => handleNavigation("/portfolio")}
+            >
+              Portfolio
+            </NavLink>
+            <NavLink
+              href="/services"
+              className="navLink"
+              onClick={() => handleNavigation("/services")}
+            >
+              Services
+            </NavLink>
+            <NavLink
+              href="/blog"
+              className="navLink"
+              onClick={() => handleNavigation("/blog")}
+            >
+              Blog
+            </NavLink>
+            <NavLink
+              href="/about"
+              className="navLink"
+              onClick={() => handleNavigation("/about")}
+            >
+              About Me
+            </NavLink>
+            <NavLink
+              href="/contact"
+              className="navLink"
+              onClick={() => handleNavigation("/contact")}
+            >
               Contact
             </NavLink>
           </nav>
